2ea4d25b6f7500511c84d79d6868f0c8c89ae2ee
monoclecat <git@andrewdelay.com>
2021-08-06 13:04:09 +0200

    Add run_experiment option to set `interval_max_JINFO`

--- run_experiment.py
+++ run_experiment.py
@@ -9,11 +9,14 @@ if __name__ == '__main__':
     parser.add_argument('--env', required=True)
     parser.add_argument('--num_disc', type=int, required=True)
     parser.add_argument('--num_cont', type=int, required=True)
+    parser.add_argument('--d_info', type=int)
     args = parser.parse_args()
 
     eg = ExperimentGrid(name='exp_grid')
     eg.add('env_name', args.env, '', True)
     eg.add('num_disc_skills', args.num_disc, 'disc', True)
     eg.add('num_cont_skills', args.num_cont, 'cont', True)
+    if args.d_info is not None:
+        eg.add('interval_max_JINFO', args.d_info, 'dinfo', True)
     eg.add('seed', [10*i for i in range(args.num_seeds)])
     eg.run(eval('spinup.lsac_pytorch'))
