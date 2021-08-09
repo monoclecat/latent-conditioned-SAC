86e35d053b75b52025dcb9eb1027ebcacb8e5a3f
monoclecat <git@andrewdelay.com>
2021-08-09 14:46:08 +0200

    Of course the reward function for SAC must be the same as for LSAC

--- run_sac_experiment.py
+++ run_sac_experiment.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('--env', required=True)
     args = parser.parse_args()
 
-    eg = ExperimentGrid(name='exp_grid')
+    eg = ExperimentGrid(name='exp_grid_sac_')
     eg.add('env_name', args.env, '', True)
     eg.add('seed', [10*i for i in range(args.num_seeds)])
     eg.run(eval('spinup.sac_pytorch'))
